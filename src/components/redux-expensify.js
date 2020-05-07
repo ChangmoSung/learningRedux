9a0a7d0a091913da46ad158707680cff1bdd790e
ChangmoSung <changmosung0914@gmail.com>
2020-05-07 02:14:11 -0400

    practicing reducer

--- src/components/redux-expensify.js
+++ src/components/redux-expensify.js
@@ -1,9 +1,38 @@
 import { createStore, combineReducers } from 'redux';
+import {v1 as uuid} from 'uuid';
+
+const addExpense = ({ 
+    description = '',
+    note = '',
+    amount = 0,
+    createdAt = 0 
+} = {}) => ({
+    type: 'ADD_EXPENSE',
+    expense: {
+        id: uuid(),
+        description,
+        note,
+        amount,
+        createdAt,
+    }
+})
+
+const removeExpense = ({id} = {}) => ({
+    type: 'REMOVE_EXPENSE',
+    id
+})
 
 const expensesReducerDefaultState = []
 
 const expensesReducer = (state = expensesReducerDefaultState, action) => {
     switch(action.type) {
+        case 'ADD_EXPENSE':
+            return [
+                ...state,
+                action.expense
+            ]
+        case 'REMOVE_EXPENSE':
+            return state.filter(({id}) => id !== action.id);
         default: 
             return state;
     }
@@ -30,22 +59,30 @@ const store = createStore(
     })
 );
 
-console.log(store.getState())
+store.subscribe(() => {
+    console.log(store.getState())
+})
 
-const demoState = {
-    expenses: [{
-        id: 'sdfsdfsd',
-        description: 'January Rent',
-        note: 'This was the final payment for that address',
-        amount: 54500,
-        createdAt: 0
-    }],
-    filters: {
-        text: 'rent',
-        sortBy: 'amout',
-        startDate: undefined,
-        endDate: undefined
-    }
-};
+const expenseOne = store.dispatch(addExpense({ description: 'rent', amout: 100 }));
+
+const expenseTwo = store.dispatch(addExpense({ description: 'coffee', amout: 300}));
+
+store.dispatch(removeExpense({id: expenseOne.expense.id}))
+
+// const demoState = {
+//     expenses: [{
+//         id: 'sdfsdfsd',
+//         description: 'January Rent',
+//         note: 'This was the final payment for that address',
+//         amount: 54500,
+//         createdAt: 0
+//     }],
+//     filters: {
+//         text: 'rent',
+//         sortBy: 'amout',
+//         startDate: undefined,
+//         endDate: undefined
+//     }
+// };
 
 export default store;
